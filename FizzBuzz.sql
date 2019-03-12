5f3de5ad3099dff73056c44929d1156268a9fd5a
Christopher Stoll <43278229+chris-stoll@users.noreply.github.com>
2019-03-12 14:25:43 -0500

    Create
    Create

---
+++ FizzBuzz.sql
@@ -0,0 +1,9 @@
+;WITH num (n)
+AS
+(
+	SELECT 1
+	UNION ALL
+    SELECT n+1 FROM num WHERE n < 100
+)
+SELECT 'n' = ISNULL(NULLIF(CASE WHEN n%3=0 THEN 'Fizz' ELSE '' END + CASE WHEN n%5=0 THEN 'Buzz' ELSE '' END,''),CAST(n AS VARCHAR(3)))
+FROM num;
