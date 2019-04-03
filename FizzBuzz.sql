2ed2eda0770638d14252155f04becd2083c497d6
Chris Stoll <chrisstoll@gmail.com>
2019-04-02 20:39:46 -0500

    Removed cast to varchar on results.

--- FizzBuzz.sql
+++ FizzBuzz.sql
@@ -1,9 +1,12 @@
-;WITH num (n)
-AS
-(
-	SELECT 1
-	UNION ALL
-    SELECT n+1 FROM num WHERE n < 100
-)
-SELECT 'n' = ISNULL(NULLIF(CASE WHEN n%3=0 THEN 'Fizz' ELSE '' END + CASE WHEN n%5=0 THEN 'Buzz' ELSE '' END,''),CAST(n AS VARCHAR(3)))
+;WITH
+	num (n)
+	AS
+	(
+					SELECT 1
+		UNION ALL
+			SELECT n+1
+			FROM num
+			WHERE n < 100
+	)
+SELECT 'n' = ISNULL(NULLIF(CASE WHEN n%3=0 THEN 'Fizz' ELSE '' END + CASE WHEN n%5=0 THEN 'Buzz' ELSE '' END,''),n)
 FROM num;
