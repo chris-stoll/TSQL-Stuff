cd37965c9909bf23d3e77a8d0d827f4353d6de03
Chris Stoll <chrisstoll@gmail.com>
2019-04-30 07:35:24 -0500

    FizzBuzz Formatting

--- FizzBuzz.sql
+++ FizzBuzz.sql
@@ -2,11 +2,11 @@
 	num (n)
 	AS
 	(
-					SELECT 1
+		SELECT 1
 		UNION ALL
-			SELECT n+1
-			FROM num
-			WHERE n < 100
+		SELECT n+1
+		FROM num
+		WHERE n < 100
 	)
 SELECT 'n' = ISNULL(NULLIF(CASE WHEN n%3=0 THEN 'Fizz' ELSE '' END + CASE WHEN n%5=0 THEN 'Buzz' ELSE '' END,''),n)
 FROM num;
