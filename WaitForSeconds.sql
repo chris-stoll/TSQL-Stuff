867875be60ff110a8148b8e0d7b83c74c78507a6
chrisstoll <chrisstoll@gmail.com>
2019-05-09 19:58:30 -0500

    Made WaitForSeconds less dumb

--- WaitForSeconds.sql
+++ WaitForSeconds.sql
@@ -17,18 +17,10 @@ BEGIN
 		SELECT @Duration = ABS(CHECKSUM(NEWID())) % @Duration;
 	END
 
-	DECLARE @Hours SMALLINT = @Duration / 3600;
-	SELECT @Duration = @Duration % 3600;
-	DECLARE @Minutes TINYINT = @Duration / 60;
-	SELECT @Duration = @Duration % 60;
-	DECLARE @Seconds TINYINT = @Duration;
-    
-	DECLARE @WaitFor VARCHAR(50) = 'WAITFOR DELAY ''{H}:{M}:{S}'' ';
-
-	SELECT @WaitFor = REPLACE(@WaitFor, '{H}', cast(ISNULL(@Hours, '00') AS VARCHAR(4)));
-	SELECT @WaitFor = REPLACE(@WaitFor, '{M}', cast(ISNULL(@Minutes, '00') AS VARCHAR(2)));
-	SELECT @WaitFor = REPLACE(@WaitFor, '{S}', cast(ISNULL(@Seconds, '00') AS VARCHAR(2)));
-	
-	EXEC(@WaitFor);
+	--PRINT CAST(@Duration as varchar(50));
+	DECLARE @WaitUntilTime DATETIME;
+	SELECT @WaitUntilTime = DATEADD(SECOND, @Duration, GETDATE())
+	--PRINT CAST(@WaitUntilTime as varchar(50));
+	WAITFOR TIME @WaitUntilTime	
 END
 GO
