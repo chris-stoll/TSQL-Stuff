caf7fd6b0fa36c0e53dc6f850bf10c6220fc84fe
chrisstoll <chrisstoll@gmail.com>
2019-11-08 10:04:00 -0600

    Fixed @Outputstring size

--- nGramSplitter.sql
+++ nGramSplitter.sql
@@ -4,6 +4,11 @@ RETURNS @output TABLE
 OrderID SMALLINT NOT NULL PRIMARY KEY IDENTITY(1,1), ngram VARCHAR(MAX)
 
 )
+/*
+	Splits sentences into ngrams
+	Created by: Chris Stoll
+	Created on: 11/08/2019
+*/
 AS
 /*
 select OrderID, ngram
@@ -16,7 +21,7 @@ from dbo.SplitNGram('The quick brown fox jumps over the lazy dog .', 2);
 BEGIN
 	DECLARE @SplitString TABLE (OrderID SMALLINT NOT NULL PRIMARY KEY IDENTITY(1,1), Word VARCHAR(MAX));
 	DECLARE @Delimiter CHAR(1) = ' ';
-	DECLARE @outputstring VARCHAR(500);
+	DECLARE @outputstring VARCHAR(MAX);
 
 	INSERT INTO @SplitString(Word)
 	SELECT ss.value
