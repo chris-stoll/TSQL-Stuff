7038ff53ffeca654f00d1027db30d6c5bcb71110
chrisstoll <chrisstoll@gmail.com>
2019-11-11 14:46:34 -0600

    Added remove trailing character function

---
+++ RemoveTrailingCharacter.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION dbo.RemoveTrailingCharacter(@input VARCHAR(MAX), @character CHAR(1))
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+	RETURN CASE
+				WHEN RIGHT(RTRIM(@input),1) = @character 
+				THEN SUBSTRING(RTRIM(@input),1,LEN(RTRIM(@input))-1)
+				ELSE @input
+		   END
+END
