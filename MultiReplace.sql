f0d17f9138b0bcf696959caa8bdbc1e320a27a4f
chrisstoll <chrisstoll@gmail.com>
2021-05-17 21:33:15 -0500

    Added MultiReplace

---
+++ MultiReplace.sql
@@ -0,0 +1,19 @@
+CREATE OR ALTER FUNCTION dbo.MutliReplace
+(
+    @InputString NVARCHAR(MAX)
+    ,@TargetString NVARCHAR(MAX) 
+    ,@ReplacementChar CHAR(1) 
+)
+RETURNS NVARCHAR(MAX)
+/*
+
+SELECT dbo.MutliReplace('TEST!@#$%^&*()TEST', '!@#$%^&*()', ' ')
+
+*/
+AS
+BEGIN
+RETURN
+	REPLACE(TRANSLATE(@InputString, @TargetString, REPLICATE(CHAR(26), LEN(@TargetString))), CHAR(26), @ReplacementChar)
+
+END
+GO
